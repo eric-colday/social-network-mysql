7826ce968683a5a5ee2fa3b280ccb6288e634615
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2024-03-10 13:02:26 +0100

    cacher les élément de connexion

--- connect.js
+++ connect.js
@@ -1,10 +1,21 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 
 export const db = mysql.createConnection({
-  port: "8889",
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "test",
+  port: Number(process.env.DB_PORT),
+  host: String(process.env.DB_HOST),
+  user: String(process.env.DB_USER),
+  password: String(process.env.DB_PASSWORD),
+  database: String(process.env.DB_DATABASE),
 });
+
+// export const db = mysql.createConnection({
+//   port: "8889",
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "test",
+// });
